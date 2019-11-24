5c16948891ea4bcac9601a0daa9edb240b23b5af
CoryB <cory@goworks.xyz>
2019-11-24 10:19:28 -0800

    Added query

---
+++ StupifyText.psm1
@@ -0,0 +1,38 @@
+function Stupify-String {
+    [CmdletBinding()]
+    param (
+        # Text to stupify
+        [Parameter(Mandatory)]
+        [string]
+        $String
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        $Chars = $String.ToCharArray()
+        [System.Func[char, System.Object]] $SelectDelegate = {
+            param (
+                [Parameter(Mandatory)]
+                [char]
+                $c
+            )
+            $Rando = Get-Random -Minimum 0 -Maximum 9
+            if ($Rando -lt 5) {
+                [System.Char]::ToLowerInvariant($c)
+            }
+            else {
+                [System.Char]::ToUpperInvariant($c)
+            }
+        }
+        $StupidQuery = [System.Linq.Enumerable]::Select($Chars, $SelectDelegate)
+        $StupidArray = [System.Linq.Enumerable]::ToArray($StupidQuery)
+        
+    }
+    
+    end {
+        
+    }
+}
