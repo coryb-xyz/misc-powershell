4b3b2bc4120f4e6398f65c7a65ab19f51933a2b9
CoryB <cory@goworks.xyz>
2019-12-01 12:17:59 -0800

    Fixed Imports

---
+++ csharp/CdbXyzCmdlets/MyCmdlet/GetRandomName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace MyCmdlet
+{
+    [Cmdlet(VerbsCommon.Get, "RandomName")]
+    public class GetRandomName : Cmdlet
+    {
+
+    }
+}
