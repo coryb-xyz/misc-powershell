2c8b7787a376cf6d3f350dec31206e9fb958c167
CoryB <cory@goworks.xyz>
2019-12-01 12:28:50 -0800

    first logic in cmdlet

--- csharp/CdbXyzCmdlets/MyCmdlet/GetRandomName.cs
+++ csharp/CdbXyzCmdlets/MyCmdlet/GetRandomName.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,15 @@ using System.Management.Automation;
 namespace MyCmdlet
 {
     [Cmdlet(VerbsCommon.Get, "RandomName")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
     public class GetRandomName : Cmdlet
     {
+        [Parameter(Position = 1, Mandatory = true, ValueFromPipeline = true)]
+        public string Name { get; set; }
 
+        protected override void ProcessRecord()
+        {
+            Console.WriteLine($"{Name}, len: {Name.Length}");
+        }
     }
 }
