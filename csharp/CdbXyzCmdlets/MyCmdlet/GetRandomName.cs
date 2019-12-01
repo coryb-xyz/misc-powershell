e034225779c3efdc3e730ee7a1b3453b8967108f
CoryB <cory@goworks.xyz>
2019-12-01 13:00:57 -0800

    Loading external files working

--- csharp/CdbXyzCmdlets/MyCmdlet/GetRandomName.cs
+++ csharp/CdbXyzCmdlets/MyCmdlet/GetRandomName.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management.Automation;
+using System.Reflection;
 using System.Threading;
 
 namespace MyCmdlet
@@ -13,24 +15,30 @@ namespace MyCmdlet
     [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
     public class GetRandomName : Cmdlet
     {
+        private string[] _names;
+
         [Parameter(Position = 1, Mandatory = true, ValueFromPipeline = true)]
         public string Name { get; set; }
 
         protected override void BeginProcessing()
         {
+            WriteVerbose("Loading files");
+            _names = File.ReadAllLines(
+                Path.Combine(
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new InvalidOperationException(),
+                    "GetRandomName.txt"
+                    )
+                );
         }
 
         protected override void ProcessRecord()
         {
             WriteVerbose(Name);
-            var nameChars = Name.ToCharArray();
-            Array.Reverse(nameChars);
-            WriteObject(new
-            {
-                ReversedName= new string(nameChars),
-                NameLength = Name.Length
-
-            });
+           WriteObject(
+               _names.Where(n => n.Length == Name.Length)
+                   .OrderBy(n => Guid.NewGuid())
+                   .FirstOrDefault()
+               );
         }
 
         protected override void EndProcessing()
